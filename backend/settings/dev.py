0accc1bbbd43b0ae3d151fb0d11ae9a3b5647333
Jeffrey A. Clark (Alex) <aclark@aclark.net>
2024-04-12 13:34:54 -0400

    Update project-makefile

---
+++ backend/settings/dev.py
@@ -0,0 +1,22 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nutn0t_@i5k@l7epb+1t70knicw!#o!m#j)2@x%q^u_31bo3rf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+MIDDLEWARE.append('hijack.middleware.HijackUserMiddleware')
+INTERNAL_IPS = ["127.0.0.1",]
